b12ce1a379d9c7504e6c8feff543ca2124574724
Anurodh <anurodhp7@gmail.com>
2024-09-08 10:23:19 +1000

    errorHandler

---
+++ backend/src/middlewares/errorHandler.js
@@ -0,0 +1,16 @@
+// backend/src/middlewares/errorHandler.js
+
+const errorHandler = (err, req, res, next) => {
+  console.error(err.stack);
+
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Something went wrong on the server";
+
+  res.status(statusCode).json({
+    status: "error",
+    statusCode,
+    message,
+  });
+};
+
+module.exports = errorHandler;
