7e1a98f34932faa4d4fb7c99344331b285b21a6d
Anurodh <anurodhp7@gmail.com>
2024-09-08 12:31:43 +1000

    fix API_URL

--- frontend/src/services/apiService.js
+++ frontend/src/services/apiService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api";
+const API_URL =
+  import.meta.env.VITE_BACKEND_API_URL || "http://localhost:3000/api";
 
 export const interpretParkingSign = async (imageData) => {
   try {
