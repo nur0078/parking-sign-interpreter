6bf9fc741096aefeea2dab37ef947dd4c676476e
Anurodh <anurodhp7@gmail.com>
2024-09-08 12:31:27 +1000

    expose server

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0", // Expose to all network interfaces
+    port: 5173, // You can keep the default port or change it as needed
+  },
+});
